0e976e80940d3ec691bbe1b7b9379ca319affc37
Gabriel <gabrielgvo16@gmail.com>
2025-08-28 22:00:03 -0300

    add formsubmit

---
+++ docs/script.js
@@ -0,0 +1,6 @@
+const form = document.getElementById("contact-form");
+const msg = document.getElementById("success-msg");
+
+form.addEventListener("submit", function() {
+    msg.style.display="block";
+});
